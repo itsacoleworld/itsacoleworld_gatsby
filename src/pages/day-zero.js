3d307127a89ae8ec2c6533fc577ed5dafb0b2fd7
Alex Coleman <alexcoleman@Alexs-MBP.fios-router.home>
2020-03-13 08:50:58 -0400

    add day zero folder

---
+++ src/pages/day-zero.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const DayZeroPage = () => (
+  <Layout>
+    <SEO title="Day Zero - itsacoleworld" />
+    <h1>Day Zero</h1>
+    <p>Welcome to Day Zero</p>
+  </Layout>
+)
+
+export default DayZeroPage
