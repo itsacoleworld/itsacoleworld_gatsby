ccf62d638bfea24ce254f4f1541bab8408e837f0
Alex Coleman <alexcoleman@Alexs-MacBook-Pro.local>
2020-03-16 12:40:15 -0400

    remove sections without content yet

--- src/components/header.js
+++ src/components/header.js
@@ -8,19 +8,13 @@ const Header = ({ siteTitle }) => (
       <h3>{siteTitle}</h3>
         <ul>
             <li>
+                <Link to="/about/">About</Link>
+            </li>
+            <li>
                 <Link to="/day-zero/">Day Zero</Link>
             </li>
             <li>
                 <Link to="/books/">Books</Link>
-            </li>
-            <li>
-                <Link to="/about/">About</Link>
-            </li>
-            <li>
-                <Link to="/projects/">Projects</Link>
-            </li>
-            <li>
-                <Link to="/writing/">Writing</Link>
             </li>
         </ul>
     </div>
